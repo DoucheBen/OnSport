594ec946afcc6debac7ce4c8b1de24195e843758
isidore18 <adrien.kowalczyk@gmail.com>
2017-09-12 11:24:28 +0200

    adri

---
+++ app/src/main/java/cdi/com/onsport/userAccount/MonCompte.java
@@ -0,0 +1,15 @@
+package cdi.com.onsport.userAccount;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cdi.com.onsport.R;
+
+public class MonCompte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mon_compte);
+    }
+}
